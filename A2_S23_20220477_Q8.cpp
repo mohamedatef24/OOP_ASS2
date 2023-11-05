b9f6b1c1875a762c63708449c1164f379680d432
mohamedosama25 <147179514+mohamedosama25@users.noreply.github.com>
2023-11-05 20:13:27 +0200

    Update A2_S23_20220477_Q8.cpp
    added q8

--- A2_S23_20220477_Q8.cpp
+++ A2_S23_20220477_Q8.cpp
@@ -4,38 +4,38 @@
 // Author: Mohamed Osama
 // Section: S23
 // ID: 20220477
-// TA: ……………………
+// TA: Mohamed Talaat
 // Date: 3 Nov 2023
 
 #include <iostream>
 using namespace std;
 
-// n: number of stars in the middle row
-// i: number of spaces before the pattern
-void pattern(ostream& outs, unsigned int n, unsigned int i){
+void Pattern(int spaces, int length)
+{
+    // Base case: If the length is zero, stop the recursion.
+    if (length == 0)
+        return;
 
-    if (n == 1){ // Base case: if n is 1, print a single star and return
-        outs << "*"<<endl;
-    }
+    // Recursive call to draw the "half pattern" above the current line.
+    Pattern(spaces, length / 2);
 
-    else{ // Recursive case: print the pattern recursively
-        pattern(outs, n / 2, i + 1); // Print the pattern for the top half
-        for (int k = 0; k < n; k++){ // Print the middle row of stars
-            outs << "* ";
+    // Print spaces to create an indentation, and then print '*' characters to form the central string.
+    for (int i = 0; i < spaces; i++) cout << "  ";
+    for (int i = 0; i < length; i++) cout << "* ";
+    cout << endl;
 
-        }
-        outs<<endl;
-
-        for (int k = 0; k < i; k++){ // Print the spaces before the pattern
-            outs << ' ';
-        }
-
-        pattern(outs, n / 2, i + 1); // Print the pattern for the bottom half
-
-    }
+    // Recursive call to draw the "half pattern" below the current line.
+    Pattern(spaces + length / 2, length / 2);
 }
 
-int main() {
-    pattern(cout,8,2); // Call the pattern function with n=8 and i=2
-    return 0;
+int main()
+{
+    // n is the number that the user will enter
+    int n;
+    cout << "Enter a number that is power of 2: ";
+    cin >> n;
+
+    // Call the pattern drawing function, starting with zero spaces and the specified 'n'.
+    Pattern(0, n);
+    // in case of n = 8 here the program will print the shape in the question
 }
